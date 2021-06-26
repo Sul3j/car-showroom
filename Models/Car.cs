d288b5319f6ba0498c53e7a87191116fda409277
Szymon Sulejczak <55898132+Sul3j@users.noreply.github.com>
2021-06-26 08:17:38 +0200

    ADD: Car table model

---
+++ Models/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShowroom.Models
+{
+    public class Car
+    {
+        [Key]
+        public int CarId { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public string Mark { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public string Model { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string ProductionDate { get; set; }
+
+        [Column(TypeName = "nvarchar(17)")]
+        public string VIN { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string CarRegistration { get; set; }
+
+        [Column(TypeName = "nvarchar(4)")]
+        public string CarPower { get; set; }
+    }
+}
