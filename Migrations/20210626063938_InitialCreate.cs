37610d744b5c7d870f52735e57c1d138704f81cf
Szymon Sulejczak <55898132+Sul3j@users.noreply.github.com>
2021-06-26 08:43:30 +0200

    ADD: Connection string on Startup.cs and migration

---
+++ Migrations/20210626063938_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarShowroom.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    CarId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Mark = table.Column<string>(type: "nvarchar(20)", nullable: true),
+                    Model = table.Column<string>(type: "nvarchar(20)", nullable: true),
+                    ProductionDate = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    VIN = table.Column<string>(type: "nvarchar(17)", nullable: true),
+                    CarRegistration = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    CarPower = table.Column<string>(type: "nvarchar(4)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.CarId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using CarShowroom.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,8 @@ namespace CarShowroom
         {
 
             services.AddControllers();
+            services.AddDbContext<CarContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
