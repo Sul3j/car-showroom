12afb8506b865126dc63be3a9c11b7f8b8c5e948
Szymon Sulejczak <55898132+Sul3j@users.noreply.github.com>
2021-06-26 10:53:23 +0200

    ADD: frontend structure

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarsComponent } from './cars/cars.component';
+import { CarsFormComponent } from './Cars/cars-form/cars-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent,
+    CarsFormComponent
   ],
   imports: [
     BrowserModule
