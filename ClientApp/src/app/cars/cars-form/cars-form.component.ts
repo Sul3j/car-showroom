f6b7af7776c63ce29695e406422f1b54e5932c33
Szymon Sulejczak <55898132+Sul3j@users.noreply.github.com>
2021-06-27 13:03:04 +0200

    ADD: reset form method

--- ClientApp/src/app/cars/cars-form/cars-form.component.ts
+++ ClientApp/src/app/cars/cars-form/cars-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Cars } from 'src/app/shared/cars.model';
 import { CarsService } from 'src/app/shared/cars.service';
 
 @Component({
@@ -18,10 +19,15 @@ export class CarsFormComponent implements OnInit {
   onSubmit(form: NgForm){
     this.service.postCars().subscribe(
       res => {
-
+        this.resetForm(form);
       },
       err => { console.log(err); }
     );
+  }
+
+  resetForm(form: NgForm){
+    form.form.reset();
+    this.service.formData = new Cars();
   }
 
 }
